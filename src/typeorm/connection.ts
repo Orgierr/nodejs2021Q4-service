17b99a9a2c00375675714688cc957e178a8a90f1
Igor.K <infinitemind625@gmail.com>
2022-01-05 18:48:37 +0200

    refactor:pg connection

--- src/typeorm/connection.ts
+++ src/typeorm/connection.ts
@@ -1,8 +1,19 @@
-import { createConnection } from 'typeorm';
+// import { createConnection } from 'typeorm';
 import { config } from '../common/config';
+
+// createConnection({
+//   type: 'postgres',
+//   url: `postgres://${config.POSTGRES_USER}:${config.POSTGRES_PASSWORD}@${config.POSTGRES_HOST}/${config.POSTGRES_DB}`,
+// }).then((_) => console.log('Postgres connected'));
+
+import { Client } from 'pg';
 import { logger } from '../logger/logger';
 
-createConnection({
-  type: 'postgres',
-  url: `postgres://${config.POSTGRES_USER}:${config.POSTGRES_PASSWORD}@${config.POSTGRES_HOST}/${config.POSTGRES_DB}`,
-}).then((_) => logger.info(`'Postgres connected'`));
+const client = new Client({
+  host: config.POSTGRES_HOST,
+  port: Number(config.POSTGRES_PORT),
+  user: config.POSTGRES_USER,
+  password: config.POSTGRES_PASSWORD,
+});
+
+client.connect().then((_) => logger.info('Postgres connected'));
