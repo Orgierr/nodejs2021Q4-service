726339e2c2257577644f61e3dc6869b8c74cd0fb
Igor.K <infinitemind625@gmail.com>
2022-01-10 00:38:53 +0200

    init:start task8

--- src/typeorm/connection.ts
+++ src/typeorm/connection.ts
@@ -1,19 +1,7 @@
-// import { createConnection } from 'typeorm';
+import { createConnection } from 'typeorm';
 import { config } from '../common/config';
 
-// createConnection({
-//   type: 'postgres',
-//   url: `postgres://${config.POSTGRES_USER}:${config.POSTGRES_PASSWORD}@${config.POSTGRES_HOST}/${config.POSTGRES_DB}`,
-// }).then((_) => console.log('Postgres connected'));
-
-import { Client } from 'pg';
-import { logger } from '../logger/logger';
-
-const client = new Client({
-  host: config.POSTGRES_HOST,
-  port: Number(config.POSTGRES_PORT),
-  user: config.POSTGRES_USER,
-  password: config.POSTGRES_PASSWORD,
-});
-
-client.connect().then((_) => logger.info('Postgres connected'));
+createConnection({
+  type: 'postgres',
+  url: `postgres://${config.POSTGRES_USER}:${config.POSTGRES_PASSWORD}@${config.POSTGRES_HOST}/${config.POSTGRES_DB}`,
+}).then((_) => console.log('Postgres connected'));
