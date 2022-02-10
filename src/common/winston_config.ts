f408d7a2dfe01176a62484daaa17035729775db9
Igor.K <infinitemind625@gmail.com>
2022-02-10 16:06:48 +0200

    refactor:winsot config

--- src/common/winston_config.ts
+++ src/common/winston_config.ts
@@ -1,5 +1,6 @@
 import * as winston from 'winston';
 import { config } from './config';
+import { utilities } from 'nest-winston';
 export const winstonConfig = {
   level: config.LOG_LEVEL || 'debug',
   levels: {
@@ -11,20 +12,21 @@ export const winstonConfig = {
   },
   format: winston.format.combine(
     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
-    winston.format.printf(
-      (info) => `${info['timestamp']} ${info.level}: ${info.message}`,
-    ),
     winston.format.json(),
   ),
   transports: [
     new winston.transports.Console({
-      format: winston.format.combine(winston.format.colorize({ all: true })),
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        utilities.format.nestLike(),
+        winston.format.colorize({ all: true }),
+      ),
     }),
+    new winston.transports.File({ filename: 'logs/all.log' }),
     new winston.transports.File({
       filename: 'logs/error.log',
       level: 'error',
     }),
-    new winston.transports.File({ filename: 'logs/all.log' }),
     new winston.transports.File({
       filename: 'logs/exceptions.log',
       level: 'error',
